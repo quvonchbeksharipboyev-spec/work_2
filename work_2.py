c40de7f3107ba8d9938221897590e5d1a3fd8add
quvonchbeksharipboyev-spec <quvonchbeksharipboyev@gmail.com>
2026-03-06 09:06:05 +0300

    Add files via upload

---
+++ work_2.py
@@ -0,0 +1,43 @@
+#9
+# import math
+# a=int(input("a="))
+# b=int(input("b="))
+# print("geometrigini topish=", math.sqrt(a*b))
+#10
+# a=int(input("a="))
+# b=int(input("b="))
+# print("yig'indisi", a+b)
+# print("ko'paytmasi=", a*b)
+# print("a ning kvadrati=", a**2," b ning kvadrati=", b**2)
+#11
+# a=int(input("a="))
+# b=int(input("b="))
+# print("yig'indisi s=", a+b)
+# print("ko'paytmasi C=", a*b)
+# print("moduli a=", abs(a),"moduli b=", abs(b))
+#12
+# import math
+# a=int(input("a="))
+# b=int(input("b="))
+# c=int(input("c="))
+# print("perimetr P=", a+b+c )
+# print("Gipatenuza c=", math.sqrt(a**2+b**2))
+#13
+# import  math
+# R1=int(input("R1="))
+# R2=int(input("R2="))
+# print("S3=", math.pi*(R1-R2))
+#14
+# import math
+# L=float(input("L="))
+# print("Radiusi R=", L / (2 * math.pi))
+# print("Yuzasi S=", pow(L, 2)/(math.pi*4))
+#15
+# import math
+# S=float(input("S="))
+# print("Radius R=", math.sqrt(S/math.pi))
+# print("Diyametr d=", math.sqrt(4*S/math.pi))
+#16
+# x1=float(input("x1="))
+# x2=float(input("x2="))
+# print("s=", abs(x2-x1))
